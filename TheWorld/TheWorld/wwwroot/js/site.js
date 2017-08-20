4479f187872bab75e89b509e636c9124f2622766
Ulrich Nilsson <ulrich_nilsson@hotmail.com>
2017-08-20 14:54:26 +0200

    Before menu

--- TheWorld/TheWorld/wwwroot/js/site.js
+++ TheWorld/TheWorld/wwwroot/js/site.js
@@ -1,17 +1,25 @@
 ﻿// site.js
-(function() {
-    var ele = document.getElementById("username");
-    ele.innerHTML = "Test";
+(function () {
+
+    //var ele = $("#username");
+    //ele.text = "Shawn Wildermuth";
  
-    var main = document.getElementById("main");
-    main.onmouseenter = function() {
-        main.style.backgroundColor = "#888";
+    //var main = $("#main");
+    //main.on("mouseenter",  function() {
+    //    main.style.backgroundColor = "#888";
+    //});
  
-    };
+    //main.on("mouseleave" , function() {
+    //    main.style.backgroundColor = "";
  
-    main.onmouseleave = function() {
-        main.style.backgroundColor = "";
- 
- 
-    };
+    //});
+
+    //var menuItems = $("ul.menu li a");
+    //var me = $(this)
+
+    //menuItems.on("click", function () {
+    //    alert(me.text);
+    //});
+
+
 })();
